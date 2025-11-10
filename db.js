0f79537b91dc22d76ddbacedaae8ce8cc0247010
khelsyaamandaputri89-rgb <khelsyaamandaputri89@gmail.com>
2025-11-10 14:28:22 +0700

    refactoring to connect db on server

--- db.js
+++ db.js
@@ -1,37 +1,18 @@
-const { Sequelize } = require("sequelize")
-require("dotenv").config()
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const DATABASE_URL = process.env.DATABASE_URL
-let sequelize
+const DATABASE_URL = process.env.DATABASE_URL;
 
-if (DATABASE_URL) {
-   sequelize = new Sequelize(DATABASE_URL, {
-      dialect: 'postgres',
-      logging: false,
-      dialectOptions: {
-         ssl: {
-            require: true, 
-            rejectUnauthorized: false 
-         }
-      }
-   })
-} else {
-   const config = require("./src/config/config.json") ["development"]
-   sequelize = new Sequelize (
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-         host : process.env.DB_HOST,
-         port : process.env.DB_PORT,
-         dialect : "postgres",
-         logging : false
-      }
-   )
-}
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: { require: true, rejectUnauthorized: false },
+  },
+});
 
 sequelize.authenticate()
-   .then(() => console.log('koneksi dengan database berhasil'))
-   .catch(err => console.log('Error:' + err))
+  .then(() => console.log("✅ Koneksi dengan database berhasil"))
+  .catch(err => console.error("❌ Database connection failed:", err));
 
 module.exports = sequelize;
