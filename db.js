ba01995b7e3accec4ea8cbab16b53742cc3a2fd0
khelsyaamandaputri89-rgb <khelsyaamandaputri89@gmail.com>
2025-11-10 14:33:29 +0700

    connect db on server

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
-const { Sequelize } = require("sequelize");
-require("dotenv").config();
+const { Sequelize } = require("sequelize")
+require("dotenv").config()
 
-const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_URL = process.env.DATABASE_URL
+
+if (!DATABASE_URL) {
+  throw new Error("❌ DATABASE_URL is not defined!")
+}
 
 const sequelize = new Sequelize(DATABASE_URL, {
   dialect: "postgres",
