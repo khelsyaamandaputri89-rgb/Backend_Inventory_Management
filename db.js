7619b28eb58dd43d8e14c04da40c608f8eaf2cc7
khelsyaamandaputri89-rgb <khelsyaamandaputri89@gmail.com>
2025-11-11 14:56:43 +0700

    update database

--- db.js
+++ db.js
@@ -1,18 +1,21 @@
-const { Sequelize } = require("sequelize")
-require("dotenv").config()
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const isProduction = !!process.env.DATABASE_URL
+const isProduction = !!process.env.DATABASE_URL;
 
-let sequelize
+let sequelize;
 
 if (isProduction) {
-   sequelize = new Sequelize(process.env.DATABASE_URL, {
-      dialect: "postgres",
-      dialectOptions: {
-         ssl: { require: true, rejectUnauthorized: false },
-      },
-      logging: false,
-   })
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    logging: false,
+  });
 
   console.log("🟢 Mode: Production (DATABASE_URL digunakan)");
 } else {
@@ -26,20 +29,19 @@ if (isProduction) {
       dialect: "postgres",
       logging: false,
     }
-  )
+  );
   console.log("🟠 Mode: Local Development");
 }
 
 (async () => {
   try {
-    await sequelize.authenticate()
+    await sequelize.authenticate();
     console.log("✅ Koneksi ke PostgreSQL berhasil!");
-
-    await sequelize.sync({ alter: true }); 
+    await sequelize.sync({ alter: true });
     console.log("🔄 Semua model berhasil disinkronkan ke database!");
   } catch (err) {
     console.error("❌ Gagal konek ke PostgreSQL:", err.message);
   }
-})()
+})();
 
-module.exports = sequelize
+module.exports = sequelize;
