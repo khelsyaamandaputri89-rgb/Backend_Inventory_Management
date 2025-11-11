9f001ed3f7216b5fe085e88831dde8d7d5b8b998
khelsyaamandaputri89-rgb <khelsyaamandaputri89@gmail.com>
2025-11-11 11:10:55 +0700

    fix database for railway

--- db.js
+++ db.js
@@ -6,33 +6,36 @@ const DATABASE_URL = process.env.DATABASE_URL
 let sequelize
 
 if (DATABASE_URL) {
-   sequelize = new Sequelize(DATABASE_URL, {
-      dialect: 'postgres',
+  sequelize = new Sequelize(DATABASE_URL, {
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    }
+  })
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: "postgres",
       logging: false,
       dialectOptions: {
-         ssl: {
-            require: true, 
-            rejectUnauthorized: false 
-         }
+        ssl: process.env.DB_SSL === "true" ? { require: true, rejectUnauthorized: false } : false,
       }
-   })
-} else {
-   const config = require("./src/config/config.json") ["development"]
-   sequelize = new Sequelize (
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-         host : process.env.DB_HOST,
-         port : process.env.DB_PORT,
-         dialect : "postgres",
-         logging : false
-      }
-   )
+    }
+  )
 }
 
-sequelize.authenticate()
-   .then(() => console.log('koneksi dengan database berhasil'))
-   .catch(err => console.log('Error:' + err))
+sequelize
+  .authenticate()
+  .then(() => console.log("✅ Koneksi database berhasil"))
+  .catch((err) => console.error("❌ Gagal konek database:", err.message))
 
-module.exports = sequelize;
+module.exports = sequelize
