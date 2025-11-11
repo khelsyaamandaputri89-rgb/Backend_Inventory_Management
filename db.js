b5b3788f2d645baaebf4f70f87a95a9d0e98071b
khelsyaamandaputri89-rgb <khelsyaamandaputri89@gmail.com>
2025-11-11 22:47:53 +0700

    fix:update database

--- db.js
+++ db.js
@@ -1,36 +1,15 @@
-const { Sequelize } = require("sequelize");
-require("dotenv").config();
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
-const isProduction = !!process.env.DATABASE_URL;
-
-let sequelize;
-
-if (isProduction) {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-    logging: false,
-  });
+  },
+});
 
-  console.log("🟢 Mode: Production (DATABASE_URL digunakan)");
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME || "inventory_dev",
-    process.env.DB_USER || "postgres",
-    process.env.DB_PASSWORD || "postgres",
-    {
-      host: process.env.DB_HOST || "localhost",
-      port: process.env.DB_PORT || 5432,
-      dialect: "postgres",
-      logging: false,
-    }
-  );
-  console.log("🟠 Mode: Local Development");
-}
-
-module.exports = sequelize;
+export default sequelize;
