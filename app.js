2ad00686e226780c50a95b469c0fb3f099d37dac
khelsyaamandaputri89-rgb <khelsyaamandaputri89@gmail.com>
2025-11-17 14:52:57 +0700

    update app

--- app.js
+++ app.js
@@ -26,7 +26,17 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"]
 }
 
-app.use("*", cors(corsOptions))
+app.use(cors(corsOptions))
+app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS")
+     return res.sendStatus(200);
+  next()
+})
 app.use(express.json())
 
 app.get("/", (req, res) => {
