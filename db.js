a49b2eda967fb40dd681af086df76f738c0acc5f
khelsyaamandaputri89-rgb <khelsyaamandaputri89@gmail.com>
2025-11-17 13:01:38 +0700

    Uncomment production and local database configuration

--- db.js
+++ db.js
@@ -1,42 +1,37 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-// const isProduction = !!process.env.DATABASE_URL;
+const isProduction = !!process.env.DATABASE_URL;
 
-// let sequelize;
+let sequelize;
 
-// if (isProduction) {
-//   sequelize = new Sequelize(process.env.DATABASE_URL, {
-//     dialect: "postgres",
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false
-//       }
-//     },
-//     logging: false,
-//   });
+if (isProduction) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    logging: false,
+  });
 
-//   console.log("🟢 Mode: Production (DATABASE_URL digunakan)");
-// } else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME || "inventory_dev",
-//     process.env.DB_USER || "postgres",
-//     process.env.DB_PASSWORD || "postgres",
-//     {
-//       host: process.env.DB_HOST || "localhost",
-//       port: process.env.DB_PORT || 5432,
-//       dialect: "postgres",
-//       logging: false,
-//     }
-//   );
-//   console.log("🟠 Mode: Local Development");
-// }
+  console.log("🟢 Mode: Production (DATABASE_URL digunakan)");
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME || "inventory_dev",
+    process.env.DB_USER || "postgres",
+    process.env.DB_PASSWORD || "postgres",
+    {
+      host: process.env.DB_HOST || "localhost",
+      port: process.env.DB_PORT || 5432,
+      dialect: "postgres",
+      logging: false,
+    }
+  );
+  console.log("🟠 Mode: Local Development");
+}
 
-const db = new Sequelize(VITE_SERVICE_URL, {
-  define: {
-    timestamp: false
-  }
-})
 
 module.exports = db;
